c760b6a6e5ac0efdcbb3a962bcc348df812bf3cd
Shawn Campbell <he_scampbell@homesite.com>
2022-05-23 12:21:28 -0400

    Initial model creation

--- home/models.py
+++ home/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
 # Create your models here.
+class HomeStead(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Location(models.Model):
+    homestead = models.ForeignKey(HomeStead, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    primary_location = models.BooleanField()
+
+
+class Staff(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    primary_location = models.ForeignKey(Location)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    email = models.EmailField()
+
+
+class RoleClasses(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    enabled = models.BooleanField()
+
+
+class YieldUnits(models.Model):
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=3)
+
+
+class Crop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    plant_date = models.DateTimeField()
+    harvest_date = models.DateTimeField()
+    harvest_target_date = models.DateTimeField()
+    yield_goal = models.IntegerField()
+    yield_unit = models.ForeignKey(YieldUnits)
+    yield_actual = models.IntegerField()
+
+
+class Field(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    crop = models.ForeignKey(Crop)
+
