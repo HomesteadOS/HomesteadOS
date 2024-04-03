6f7ca0cda304c13a564098d5384c64d3b8e3d56e
Shawn Campbell <jaegerpicker@gmail.com>
2024-04-02 21:20:02 -0400

    Crop model and test fixes

--- home/models/crop.py
+++ home/models/crop.py
@@ -1,4 +1,8 @@
+from decimal import Decimal
 from django.contrib.gis.db import models
+from django.forms import ValidationError
+from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 from home.models.yield_units import YieldUnits
 
@@ -7,11 +11,25 @@ class Crop(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     plant_date = models.DateTimeField()
+    plant_target_date = models.DateTimeField(null=True, blank=True)
     harvest_date = models.DateTimeField()
     harvest_target_date = models.DateTimeField()
-    yield_goal = models.IntegerField()
+    yield_goal = models.DecimalField(validators=[MinValueValidator(Decimal('0.00'))], max_digits=10, decimal_places=2, default=Decimal('0.00'))
     yield_unit = models.ForeignKey(YieldUnits, on_delete=models.PROTECT)
-    yield_actual = models.IntegerField()
+    yield_actual = models.DecimalField(validators=[MinValueValidator(Decimal('0.00'))], max_digits=10, decimal_places=2, default=Decimal('0.00'))
 
     def __str__(self):
         return self.name
+
+    def clean(self, *args, **kwargs):
+        if (self.yield_actual < Decimal('0.00') 
+            or self.yield_goal < Decimal('0.00') 
+            or self.harvest_date < self.plant_date
+            or self.harvest_target_date < self.plant_date
+            or self.plant_date < timezone.now()):
+            raise ValidationError(f"Crop data. Please check your input. {self}")
+        super(Crop, self).clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Crop, self).save(*args, **kwargs)
--- home/tests/test_crop.py
+++ home/tests/test_crop.py
@@ -1,79 +1,113 @@
-import pytest
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.test import TestCase
 from django.utils import timezone
+from home.models.project import Project
+from home.tests.utils import utils
 from home.models.crop import Crop
 from home.models.yield_units import YieldUnits
 
 
-def crop_creation_with_valid_data():
-    yield_unit = YieldUnits.objects.create(name="Test Unit")
-    crop = Crop.objects.create(
-        name="Test Crop",
-        description="Test Description",
-        plant_date=timezone.now(),
-        harvest_date=timezone.now() + timezone.timedelta(days=90),
-        harvest_target_date=timezone.now() + timezone.timedelta(days=80),
-        yield_goal=1000,
-        yield_unit=yield_unit,
-        yield_actual=900
-    )
-    assert crop is not None
-
-
-def crop_string_representation():
-    yield_unit = YieldUnits.objects.create(name="Test Unit")
-    crop = Crop.objects.create(
-        name="Test Crop",
-        description="Test Description",
-        plant_date=timezone.now(),
-        harvest_date=timezone.now() + timezone.timedelta(days=90),
-        harvest_target_date=timezone.now() + timezone.timedelta(days=80),
-        yield_goal=1000,
-        yield_unit=yield_unit,
-        yield_actual=900
-    )
-    assert str(crop) == "Test Crop"
-
-
-def crop_with_harvest_date_before_plant_date():
-    with pytest.raises(ValueError):
+class CropTestCase(TestCase):
+    def test_crop_creation_with_valid_data(self):
         yield_unit = YieldUnits.objects.create(name="Test Unit")
-        Crop.objects.create(
+        crop = Crop.objects.create(
             name="Test Crop",
             description="Test Description",
-            plant_date=timezone.now(),
-            harvest_date=timezone.now() - timezone.timedelta(days=90),
+            plant_date=timezone.now() + timezone.timedelta(days=10),
+            harvest_date=timezone.now() + timezone.timedelta(days=90),
             harvest_target_date=timezone.now() + timezone.timedelta(days=80),
             yield_goal=1000,
             yield_unit=yield_unit,
             yield_actual=900
         )
+        assert crop is not None
 
 
-def crop_with_negative_yield_goal():
-    with pytest.raises(ValueError):
+    def test_crop_string_representation(self):
         yield_unit = YieldUnits.objects.create(name="Test Unit")
-        Crop.objects.create(
+        crop = Crop.objects.create(
             name="Test Crop",
             description="Test Description",
-            plant_date=timezone.now(),
-            harvest_date=timezone.now() + timezone.timedelta(days=90),
-            harvest_target_date=timezone.now() + timezone.timedelta(days=80),
-            yield_goal=-1000,
-            yield_unit=yield_unit,
-            yield_actual=900
-        )
-
-
-def crop_with_negative_yield_actual():
-    with pytest.raises(ValueError):
-        yield_unit = YieldUnits.objects.create(name="Test Unit")
-        Crop.objects.create(
-            name="Test Crop",
-            description="Test Description",
-            plant_date=timezone.now(),
+            plant_date=timezone.now() + timezone.timedelta(days=10),
             harvest_date=timezone.now() + timezone.timedelta(days=90),
             harvest_target_date=timezone.now() + timezone.timedelta(days=80),
             yield_goal=1000,
             yield_unit=yield_unit,
-            yield_actual=-900
+            yield_actual=900
         )
+        assert str(crop) == "Test Crop"
+
+
+    def test_crop_with_harvest_date_before_plant_date(self):
+        with self.assertRaises(ValidationError):
+            yield_unit = YieldUnits.objects.create(name="Test Unit")
+            Crop.objects.create(
+                name="Test Crop",
+                description="Test Description",
+                plant_date=timezone.now(),
+                harvest_date=timezone.now() - timezone.timedelta(days=90),
+                harvest_target_date=timezone.now() + timezone.timedelta(days=80),
+                yield_goal=1000,
+                yield_unit=yield_unit,
+                yield_actual=900
+            )
+
+
+    def test_crop_with_harvest_target_date_before_plant_date(self):
+        with self.assertRaises(ValidationError):
+            yield_unit = YieldUnits.objects.create(name="Test Unit")
+            Crop.objects.create(
+                name="Test Crop",
+                description="Test Description",
+                plant_date=timezone.now(),
+                harvest_date=timezone.now() + timezone.timedelta(days=90),
+                harvest_target_date=timezone.now() - timezone.timedelta(days=80),
+                yield_goal=1000,
+                yield_unit=yield_unit,
+                yield_actual=900
+            )
+            
+    
+    def test_crop_with_plant_date_in_the_past(self):
+        with self.assertRaises(ValidationError):
+            yield_unit = YieldUnits.objects.create(name="Test Unit")
+            Crop.objects.create(
+                name="Test Crop",
+                description="Test Description",
+                plant_date=timezone.now() - timezone.timedelta(days=90),
+                harvest_date=timezone.now() + timezone.timedelta(days=90),
+                harvest_target_date=timezone.now() + timezone.timedelta(days=80),
+                yield_goal=1000,
+                yield_unit=yield_unit,
+                yield_actual=900
+            )
+
+    def test_crop_with_negative_yield_goal(self):
+        with self.assertRaises(ValidationError):
+            yield_unit = YieldUnits.objects.create(name="Test Unit")
+            Crop.objects.create(
+                name="Test Crop",
+                description="Test Description",
+                plant_date=timezone.now(),
+                harvest_date=timezone.now() + timezone.timedelta(days=90),
+                harvest_target_date=timezone.now() + timezone.timedelta(days=80),
+                yield_goal=-1000,
+                yield_unit=yield_unit,
+                yield_actual=900
+            )
+
+
+    def test_crop_with_negative_yield_actual(self):
+        with self.assertRaises(ValidationError):
+            yield_unit = YieldUnits.objects.create(name="Test Unit")
+            Crop.objects.create(
+                name="Test Crop",
+                description="Test Description",
+                plant_date=timezone.now(),
+                harvest_date=timezone.now() + timezone.timedelta(days=90),
+                harvest_target_date=timezone.now() + timezone.timedelta(days=80),
+                yield_goal=1000,
+                yield_unit=yield_unit,
+                yield_actual=-900
+            )
