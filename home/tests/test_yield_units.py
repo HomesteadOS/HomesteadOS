aa6c275e9572a6c67e6c8aa58dafb00d7d650a4e
Shawn Campbell <jaegerpicker@gmail.com>
2024-04-21 18:43:22 -0400

    yeild unit test

--- home/tests/test_yield_units.py
+++ home/tests/test_yield_units.py
@@ -1,23 +1,22 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+
+import home
 from home.models.yield_units import YieldUnits
 
 
 class YieldUnitsModelTest(TestCase):
     def setUp(self):
-        self.yield_unit = YieldUnits.objects.create(name='Test YieldUnit', abbreviation='TYU')
+        home.tests.utils.utils.set_up(self)
 
-    def yield_unit_creation_with_different_name(self):
-        different_name_yield_unit = YieldUnits.objects.create(name='Different Test YieldUnit', abbreviation='DTYU')
+    def test_yield_unit_creation_with_different_name(self):
+        different_name_yield_unit = YieldUnits.objects.create(name='Different Test YieldUnit', abbreviation='DYU')
         self.assertEqual(different_name_yield_unit.name, 'Different Test YieldUnit')
 
-    def yield_unit_string_representation(self):
+    def test_yield_unit_string_representation(self):
         expected_string = self.yield_unit.name
         self.assertEqual(str(self.yield_unit), expected_string)
 
-    def yield_unit_creation_with_no_name(self):
-        with self.assertRaises(ValueError):
+    def test_yield_unit_creation_with_no_name(self):
+        with self.assertRaises(ValidationError):
             YieldUnits.objects.create(name=None, abbreviation='No Name Test Abbreviation')
-
-    def yield_unit_creation_with_same_name(self):
-        with self.assertRaises(ValueError):
-            YieldUnits.objects.create(name='Test YieldUnit', abbreviation='SYU')
